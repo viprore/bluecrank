eec2eb98be1ab01ee378d086fbb56fbf15938614
VIPRORE <lsarading@naver.com>
2017-07-24 13:16:56 +0900

    vender publish

---
+++ config/xml.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Default template
+    |--------------------------------------------------------------------------
+    |
+    | Template to XML
+    |
+    | <root xmlns:v1="http://www.site.com/schema"></root>
+    |
+    */
+    'template' => '<root></root>',
+    'caseSensitive' => false,
+    'showEmptyField' => true //Show empty field
+];
